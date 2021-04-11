e3dd311da51786831e009524af3a6d3133810e4b
Freddy Michelena <freddy@michelena.com>
2021-04-11 16:38:29 -0400

    Mejora en estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+
+// Components
+import { ProjectCardComponent } from './components/project-card/project-card.component';
+
+// Shared
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+// Pages
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { SocialComponent } from './components/social/social.component';
-import { ProjectModalComponent } from './components/project-modal/project-modal.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { AboutComponent } from './pages/about/about.component';
-import { ProjectCardComponent } from './components/project-card/project-card.component';
 
 @NgModule({
   declarations: [
@@ -21,8 +24,6 @@ import { ProjectCardComponent } from './components/project-card/project-card.com
     HomeComponent,
     ProjectsComponent,
     ContactComponent,
-    SocialComponent,
-    ProjectModalComponent,
     FooterComponent,
     AboutComponent,
     ProjectCardComponent
